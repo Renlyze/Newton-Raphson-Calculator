f0d61f8bb97fc8d438e85cc742977c4b3dc727ac
Kyell Dimatatac <ultara567@gmail.com>
2024-01-06 22:25:15 +0800

    Initial commit with new files

---
+++ src/javaapplication5/exitbtn.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication5;
+
+import javax.swing.JButton;
+
+/**
+ *
+ * @author Alysa Emilio
+ */
+public class exitbtn extends JButton {
+
+    public exitbtn(String exit) {
+    }
+    
+}
